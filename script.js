568722438bf551ed4ff1fb6fadf7309063c3e392
Urstrulyraj7 <151851746+Urstrulyraj7@users.noreply.github.com>
2024-10-12 19:50:30 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function showAlert() {
+    alert("Welcome to my portfolio website!");
+}
+
+window.onload = function() {
+    var button = document.getElementById("welcomeButton");
+    button.onclick = showAlert;
+};
